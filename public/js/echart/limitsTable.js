56806cfc606a528da0a987a78dacfb7aa8ad0a82
Rob Nightingale <rob.nightingale@reposit.be>
2016-09-25 23:31:20 +0100

    Limit table formatting

--- public/js/echart/limitsTable.js
+++ public/js/echart/limitsTable.js
@@ -50,15 +50,64 @@ $(document).ready(function() {
             { title: "Netting Set", data: "nettingSet" },
             { title: "Trade", data: "trade" },
             { title: "Metric", data: "metric" },
-            { title: "Limit Value", data: "limit" },
-            { title: "Consumption Value", data: "consumptions.0.value" },
-            { title: "Date", data: "consumptions.0.date" },
-            { title: " Cons %", data: "consumptions.0.consumption" },
+            { title: "Limit Value", data: "limit"
+                , render: $.fn.dataTable.render.number( ',', '.', 2, '$' )
+                // , render: function(data, type, row){
+                // return numeral(data).format('(0.00a)');
+            // }
+            },
+            { title: "Consumption Value", data: "consumptions.0.value"
+                , render: function(data, type, row){
+                return numeral(data).format('(0.00a)');
+            }
+            },
+            { title: "Date", data: "consumptions.0.date"
+                , render: function ( data, type, row ) {
+                // If display or filter data is requested, format the date
+                if ( type === 'display' || type === 'filter' ) {
+                    var rowvalue = row["Date"];
+                        return (moment(data, 'YYYYMMDD').format('DD-MMM-YYYY'));
+                }
+                // Otherwise the data type requested (`type`) is type detection or
+                // sorting data, for which we want to use the raw date value, so just return
+                // that, unaltered
+                return data;
+            }
+            },
+            { title: " Cons %", data: "consumptions.0.consumption"
+             , render: function(data, type, row){
+                return numeral(data).format('(0.00a)');
+            }
+            },
         ],
         deferRender: true,
         scrollY: 380,
         scrollCollapse: true,
-        scroller: true
+        scroller: true,
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: "copy",
+                className: "btn-sm"
+            },
+            {
+                extend: "csv",
+                className: "btn-sm"
+            },
+            {
+                extend: "excel",
+                className: "btn-sm"
+            },
+            {
+                extend: "pdfHtml5",
+                className: "btn-sm"
+            },
+            {
+                extend: "print",
+                className: "btn-sm"
+            },
+        ],
+
     });
 
     var $datatable = $('#datatable-checkbox');
